53a59930a7cca1fd633ac582e951761022ee5058
Wiedenmann <twd@festo.net>
2020-05-27 13:34:06 +0200

    Added Back Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,12 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot({
+      mode: 'md'
+    }),
+    AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
