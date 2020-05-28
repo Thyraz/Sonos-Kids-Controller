2fee18543c101311db182fb7452a07f784258d19
Thyraz <tobias@Tobiass-iMac-2.fritz.box>
2020-05-28 22:18:33 +0200

    Simplified server.js

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-// Get dependencies
+// Setup
 const express = require('express');
-const path = require('path');
-const http = require('http');
-
-// Get our API routes
-const api = require('./server/routes/api');
-
 const app = express();
+const path = require('path');
 
-// Parsers for POST data
+// Configuration
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Point static path to compiled ionic app
-app.use(express.static(path.join(__dirname, 'www')));
+app.use(express.static(path.join(__dirname, 'www'))); // Static path to compiled Ionic app
 
-// Set our api routes
-app.use('/api', api);
+// Models
+
+// Routes
+app.get('/api', (req, res) => {
+    res.send('api works');
+});
+
+app.get('/api/data', (req, res) => {
+    let mockData = [{
+            id: 1,
+            name: "eins"
+        },
+        {
+            id: 2,
+            name: "zwei"
+        }
+    ];
+
+    res.status(200).json(mockData);
+});
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'www/index.html'));
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = '3000';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+// listen (start app with 'node server.js')
+app.listen(8080);
+console.log("App listening on port 8080");
