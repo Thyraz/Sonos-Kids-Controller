42219965d56217f32e25aa8ec4dc9a40c39784b3
Wiedenmann <twd@festo.net>
2020-06-04 15:16:14 +0200

    Added some Todos

--- src/app/media.service.ts
+++ src/app/media.service.ts
@@ -8,10 +8,32 @@ import { Media } from './media';
 })
 export class MediaService {
 
+  media: Media[] = [];
+
   constructor(private http: HttpClient) { }
 
-  // Get all data from the API
+  // TODO: Request Caching wie hier beschrieben:
+  // https://www.syntaxsuccess.com/viewarticle/caching-with-rxjs-observables-in-angular-2.0
+
+  // Get the media data from the server
   getMedia(): Observable<Media[]> {
     return this.http.get<Media[]>('http://localhost:8200/api/data');
+  }
+
+  // TODO: Umbauen damit Observable zurück gegeben wird:
+  // https://stackoverflow.com/questions/50809542/return-an-observable-by-mapping-another-observable
+
+  // colllect all artists
+  getArtists(): Observable<string[]> {
+    this.getMedia().subscribe()
+
+    const allArtists = this.media.map(media => media.artist);
+    const uniqueArtists = Array.from(new Set(allArtists));
+
+    return uniqueArtists;
+  }
+
+  getMediaFromArtist(artist: string) {
+    return this.media.filter(media => media.artist == artist);
   }
 }
