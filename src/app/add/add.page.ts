42ac0223ad95eb8047834166a3edbda3a165d325
Wiedenmann <twd@festo.net>
2020-08-03 15:57:43 +0200

    Added onscreen keyboard

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -1,19 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MediaService } from '../media.service';
+import Keyboard from "simple-keyboard";
 
 @Component({
   selector: 'app-add',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './add.page.html',
-  styleUrls: ['./add.page.scss'],
+  styleUrls: [
+    './add.page.scss',
+    '../../../node_modules/simple-keyboard/build/css/index.css'
+  ],
 })
 export class AddPage implements OnInit {
 
   source: string = "spotify";
+  keyboard: Keyboard;
+  value="";
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit() {
+    this.keyboard = new Keyboard({
+      onChange: input => this.onChange(input),
+      onKeyPress: button => this.onKeyPress(button)
+    });
+  }
+
+  onChange = (input: string) => {
+    this.value = input;
+    console.log("Input changed", input);
+  };
+
+  onKeyPress = (button: string) => {
+    console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") this.handleShift();
+  };
+
+  onInputChange = (event: any) => {
+    this.keyboard.setInput(event.target.value);
+  };
+
+  handleShift = () => {
+    let currentLayout = this.keyboard.options.layoutName;
+    let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+    this.keyboard.setOptions({
+      layoutName: shiftToggle
+    });
+  };
 
   segmentChanged(event) {
     this.source = event.detail.value;
