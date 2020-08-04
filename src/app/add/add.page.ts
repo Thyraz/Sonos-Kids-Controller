8ea42a3c379a827c0a801341def49aa8371cf37f
Wiedenmann <twd@festo.net>
2020-08-04 16:06:19 +0200

    Optimized layout of the "Add" page

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -16,7 +16,6 @@ export class AddPage implements OnInit, AfterViewInit {
   source = 'spotify';
 
   keyboard: Keyboard;
-  value = '';
   selectedInputElem: any;
 
   constructor() { }
@@ -37,20 +36,20 @@ export class AddPage implements OnInit, AfterViewInit {
         default: [
           'q w e r t z u i o p ü',
           'a s d f g h j k l ö ä',
-          '{shift} y x c v b n m {bksp}',
-          '{alt} {space} . {enter}'
+          '{shift} y x c v b n m {shift}',
+          '{alt} {space} . {bksp}'
         ],
         shift: [
           'Q W E R T Z U I O P Ü',
           'A S D F G H J K L Ö Ä',
-          '{shiftactivated} Y X C V B N M {bksp}',
-          '{alt} {space} . {enter}'
+          '{shiftactivated} Y X C V B N M {shift}',
+          '{alt} {space} . {bksp}'
         ],
         alt: [
           '1 2 3 4 5 6 7 8 9 0 =',
           `% @ # $ & * / ( ) ' "`,
-          '{shift} , - + ; : ! ? {bksp}',
-          '{default} {space} . {enter}'
+          '{shift} , - + ; : ! ? {shift}',
+          '{default} {space} . {bksp}'
         ]
       },
       display: {
