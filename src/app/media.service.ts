43683388558b827ae8866f31759bbce3c8eb477d
Wiedenmann <twd@festo.net>
2020-08-06 10:32:47 +0200

    Code cleanup: Removed unneeded imports

--- src/app/media.service.ts
+++ src/app/media.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, from, of, iif, Subscriber, Subject } from 'rxjs';
-import { map, publishReplay, refCount, mergeMap, tap, toArray, mergeAll } from 'rxjs/operators';
+import { Observable, from, of, iif, Subject } from 'rxjs';
+import { map, mergeMap, tap, toArray, mergeAll } from 'rxjs/operators';
 import { environment } from '../environments/environment';
 import { SpotifyService } from './spotify.service';
 import { Media } from './media';
